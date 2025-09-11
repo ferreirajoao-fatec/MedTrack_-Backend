e6bb9ae7a7ed6582e83d5159b4b48f3a92ccadc4
Mariana Lourenço <marianalourencosd@gmail.com>
2025-09-11 14:12:04 -0300

    Commit: Adicionando Pagina do Formulario

---
+++ Controllers/FormularioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedTrack_Projeto.Controllers
+{
+    public class FormularioController : Controller
+    {
+        public IActionResult Formulario()
+        {
+            return View();
+        }
+    }
+}
